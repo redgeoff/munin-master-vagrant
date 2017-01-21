ad25da56c334634781269b8bbf5f39d9cb43b896
Geoff Cox <redgeoff@gmail.com>
2017-01-21 08:22:54 -0800

    initial

---
+++ apache.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+# Install Apache
+apt-get install -y apache2 apache2-utils
---
+++ dynazoom.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+# Support for dynazoom, which allows us to zoom into graphs
+apt-get install -y libcgi-fast-perl libapache2-mod-fcgid
